f21aab5011f1808751886390b0c7e75eff8ba5d2
stevericks <sricks67@gmail.com>
2023-04-19 12:50:59 +0100

    Update AzViz setup.ps1

--- AzViz setup.ps1
+++ AzViz setup.ps1
@@ -1,4 +1,25 @@
-﻿install-module -name AzViz
+# chocolatey packages Graphviz for Windows
+choco install graphviz
+
+# alternatively using windows package manager
+winget install graphviz
+
+
+#Installation
+#From PowerShell Gallery
+
+# install from powershell gallery
+Install-Module AzViz -Verbose -Scope CurrentUser -Force
+
+# import the module
+Import-Module AzViz -Verbose
+
+# login to azure, this is required for module to work
+Connect-AzAccount
+
+
+
+install-module -name AzViz
 install-module -name Az
 Import-Module AzViz
 Connect-AzAccount
