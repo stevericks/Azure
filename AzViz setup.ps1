b487b5e9d8869fa9b7ef36730aceefed611e4a14
stevericks <sricks67@gmail.com>
2023-04-19 11:17:44 +0100

    Add files via upload

---
+++ AzViz setup.ps1
@@ -0,0 +1,6 @@
+﻿install-module -name AzViz
+install-module -name Az
+Import-Module AzViz
+Connect-AzAccount
+Select-AzSubscription "sub-fgh-financier-prd"
+Export-AzViz -ResourceGroup rg-rti-uks-financier-prd -Theme light -OutputFormat svg -show
